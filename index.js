213fb14c005635b1c43719c6db5d878f5bcb8af5
Ahmad <ahmad_baq@hotmail.com>
2022-01-10 16:46:16 +0300

    completed array basics

---
+++ index.js
@@ -0,0 +1,20 @@
+let emptyArray = [];
+let hobbies = ['Cooking', 'Swimming', 'Baking', 'Video Gaming', 'Archary', 'Mountain Climbing'];
+let numbers = [4, 2, 20, 50 ,5];
+
+console.log(numbers[1]);
+console.log(hobbies[hobbies.length - 1]);
+
+hobbies.push('Dish washing and drying');
+
+console.log(hobbies[hobbies.length - 1]);
+console.log(hobbies.length);
+
+hobbies.pop();
+hobbies.pop();
+console.log(hobbies.length);
+
+console.log(hobbies);
+
+
+hobbies = [];
